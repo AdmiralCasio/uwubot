6b03dbed9e72bdafa37f9adec2a2772e181fedba
Matthew Lyon <lyonmatthew2003@hotmail.com>
2021-01-26 19:06:44 +0000

    commit

--- bot_modules/cleanmessages.js
+++ bot_modules/cleanmessages.js
@@ -1,13 +1,21 @@
-"usestrict"
-
-let botConfig = require('../config.json');
-let serverConfig = require('../server_config');
-
 let help = "**Clean**\n";
-
 let commandHandlers = {};
-const clearer = "!clear @uwubot";
 
-commandHandlers.clean = function (message, args){
-                message.channel.send(clearer);
-}
+commandHandlers.clean = function (message, args) {
+        message.channel.fetchMessages().then(messages => {
+                const botMessages = messages.filter(msg => msg.author.bot);
+                message.channel.bulkDelete(botMessages);
+            });
+};
+
+commandHandlers.wash = function (message, args) {
+        message.channel.fetchMessages().then(messages => {
+                const playerMessages = messages.filter(msg => msg.author.id);
+                message.channel.bulkDelete(playerMessages);
+            });
+};
+
+module.exports = {
+        "help": help,
+        "commandHandlers": commandHandlers
+      };
